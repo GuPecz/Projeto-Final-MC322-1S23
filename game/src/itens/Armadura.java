c65a2708f2c511ebcb4c7fdbbffc885d8964cf74
PedroBrasil111 <p260637@dac.unicamp.br>
2023-06-16 19:39:46 -0300

    implementar classe

--- game/src/itens/Armadura.java
+++ game/src/itens/Armadura.java
@@ -1,5 +1,27 @@
 package itens;
 
-public class Armadura implements Equipavel {
+public class Armadura extends Item implements Equipavel {
+    /*
+     * Atributos herdados:
+     * qtdUsos - quanto de dano a armadura ainda pode receber ate quebrar
+     */
+    private int armaduraAdicional; // armadura q deve ser adicionada a armadura do personagem
+
+    /* Construtor */
+    public Armadura(int qtdUsos, int armaduraAdicional) {
+        super(qtdUsos);
+        this.armaduraAdicional = armaduraAdicional;
+    }
+
+    /* toString()
+     * Armadura:
+     *     qtdUsos: <qtdUsos>
+     *     armaduraAdicional: <armaduraAdicional>
+     */
+    public String toString() {
+        String str = super.toString().replaceFirst("Item", "Armadura") +
+            "\n\tarmaduraAdicional: " + String.valueOf(armaduraAdicional);
+        return str;
+    }
 
 }
--- game/src/itens/Item.java
+++ game/src/itens/Item.java
@@ -1,5 +1,62 @@
 package itens;
 
-public interface Item {
+public abstract class Item {
+    private int qtdUsos; // quantidade de usos do item (esgotado se <= 0)
+    
+    /* Construtor */
+    public Item (int qtdUsos) {
+        this.qtdUsos = qtdUsos;
+    }
+
+    /*
+     * Construtor
+     * Cria item com 0 usos
+     */
+    public Item() {
+        this(0);
+    }
+
+    /*
+     * Reduz a quantidade de usos do item
+     * PARAMETROS:
+     * qtdReduzida -- quantidade a ser reduzida
+     */
+    public void reduzirUsos(int qtdReduzida) {
+        qtdUsos -= qtdReduzida;
+    }
+
+    /*
+     * Aumenta a quantidade de usos do item
+     * PARAMETROS:
+     * qtdAumentada -- quantidade a ser reduzida
+     */
+    public void aumentarUsos(int qtdAumentada) {
+        qtdUsos += qtdAumentada;
+    }
+
+    /*
+     * Retorna boolean indicando se o item nao tem mais usos
+     */
+    public boolean itemEsgotado() {
+        return qtdUsos <= 0;
+    }
+
+    /* 
+     * toString()
+     * Item:
+     *     qtdUsos: <qtdUsos>
+     */
+    public String toString() {
+        String str = "Item:\n\tqtdUsos: " + String.valueOf(qtdUsos);
+        return str;
+    }
+
+    // Getters e setters
+    public int getQtdUsos() {
+        return qtdUsos;
+    }
+    public void setQtdUsos(int qtdUsos) {
+        this.qtdUsos = qtdUsos;
+    }
 
 }
