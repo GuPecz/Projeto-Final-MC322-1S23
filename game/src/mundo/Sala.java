0d120676deb82add2549f362fa1f6abd3f225fec
Gustavo Montenari Pechta <mrpecz@proton.me>
2023-06-18 19:52:09 -0300

    implementar construtor, getters e setters

--- game/src/mundo/Sala.java
+++ game/src/mundo/Sala.java
@@ -4,6 +4,22 @@ import entidades.Inimigo;
 import itens.Item;
 
 public class Sala {
+    
+    private int id; // Índice na matriz linearizada para exibição na UI
     private Item item;
     private Inimigo inimigo;
+    
+    public Sala(int id) {
+        this.id = id;
+        this.item = null;
+        this.inimigo = null;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
