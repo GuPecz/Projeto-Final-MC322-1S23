3435b0f93cf60f6205fe476309effe0b5fede9c2
PedroBrasil111 <p260637@dac.unicamp.br>
2023-06-18 10:42:58 -0300

    adicionar nome, getters e setters

--- game/src/combate/Ataque.java
+++ game/src/combate/Ataque.java
@@ -1,5 +1,24 @@
 package combate;
 
 public class Ataque {
+    private String nome;
     private int multiplicador;
+
+    // Getters e setters
+    public String getNome() {
+        return nome;
+    }
+    
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getMultiplicador() {
+        return multiplicador;
+    }
+
+    public void setMultiplicador(int multiplicador) {
+        this.multiplicador = multiplicador;
+    }
+
 }
---
+++ game/src/combate/Magia.java
@@ -0,0 +1,15 @@
+package combate;
+
+public class Magia {
+    private String nome;
+
+    // Getters e setters
+    public String getNome() {
+        return nome;
+    }
+    
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+}
