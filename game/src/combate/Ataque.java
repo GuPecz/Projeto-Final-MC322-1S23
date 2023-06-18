54543e55b78b63d78b6346b8bad6f3801e8e527f
PedroBrasil111 <p260637@dac.unicamp.br>
2023-06-18 13:33:31 -0300

    implementar metodos

--- game/src/combate/Ataque.java
+++ game/src/combate/Ataque.java
@@ -1,8 +1,20 @@
 package combate;
 
-public class Ataque {
+import entidades.Inimigo;
+
+public abstract class Ataque {
     private String nome;
+    private int danoBase;
     private int multiplicador;
+
+    public Ataque(String nome, int danoBase, int multiplicador) {
+        this.nome = nome;
+        this.danoBase = danoBase;
+        this.multiplicador = multiplicador;
+    }
+
+    /* Retorna quanto de dano o inimigo deve receber do ataque */
+    public abstract int danoCausado(Inimigo inimigo);
 
     // Getters e setters
     public String getNome() {
@@ -11,6 +23,14 @@ public class Ataque {
     
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    public int getDanoBase() {
+        return danoBase;
+    }
+
+    public void setDanoBase(int danoBase) {
+        this.danoBase = danoBase;
     }
 
     public int getMultiplicador() {
