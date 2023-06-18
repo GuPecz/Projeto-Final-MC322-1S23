4cfba5afee338d0de13e9a2a9008eed3df6aacf9
PedroBrasil111 <p260637@dac.unicamp.br>
2023-06-18 20:05:08 -0300

    add heranca Acao

--- game/src/combate/Ataque.java
+++ game/src/combate/Ataque.java
@@ -1,28 +1,33 @@
 package combate;
 
-import entidades.Inimigo;
+import entidades.Personagem;
 
-public abstract class Ataque {
-    private String nome;
+public class Ataque extends Acao {
     private int danoBase;
 
-    public Ataque(String nome, int danoBase) {
-        this.nome = nome;
+    public Ataque(String nome, boolean habilitado, String msgUso, int danoBase) {
+        super(nome, habilitado, msgUso);
         this.danoBase = danoBase;
     }
 
-    /* Retorna quanto de dano o inimigo deve receber do ataque */
-    public abstract int danoCausado(Inimigo inimigo);
+    /* Retorna quanto de dano o alvo deve receber do ataque */
+    public int danoCausado(Personagem alvo) {
+        return (int) (danoBase * (100. / (100 + alvo.getDef())));
+    }
+
+    @Override
+    public void usar(Personagem usuario, Personagem recebedor) {
+                
+    }
+
+    // toString()
+    public String toString() {
+        String str = super.toString().replaceFirst("Acao", "Ataque");
+        str += "\n\tdanoBase: " + danoBase;
+        return str;
+    }
 
     // Getters e setters
-    public String getNome() {
-        return nome;
-    }
-    
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
     public int getDanoBase() {
         return danoBase;
     }
@@ -30,5 +35,7 @@ public abstract class Ataque {
     public void setDanoBase(int danoBase) {
         this.danoBase = danoBase;
     }
+
+
 
 }
--- game/src/combate/Magia.java
+++ game/src/combate/Magia.java
@@ -7,8 +7,8 @@ public class Magia extends Ataque {
     private String elemento;
     private int custoMp;
 
-    public Magia(String nome, int danoBase, String elemento, int custoMp) {
-        super(nome, danoBase);
+    public Magia(String nome, String msgUso, int danoBase, String elemento, int custoMp) {
+        super(nome, false, msgUso, danoBase); // inicia desabilitada (ate achar scroll)
         this.elemento = elemento;
         this.custoMp = custoMp;
     }
@@ -16,13 +16,11 @@ public class Magia extends Ataque {
     public int danoCausado(Inimigo inimigo) {
         double multiplicador = CsvHandler.getMultiplicador(elemento, inimigo.getElemento());
         if (multiplicador != 0.) { // retorno padrao da funcao quando nao encontra o elemento
-            // roubei do lol -- https://leagueoflegends.fandom.com/wiki/Magic_resistance?so=search
+            // TODO - roubei do lol -- https://leagueoflegends.fandom.com/wiki/Magic_resistance?so=search
             return (int) ((getDanoBase() * multiplicador) * (100. / (100 + inimigo.getDef())));
         }
         return 0;
     }
-
-
 
     // Getters e setters
     public String getElemento() {
