91a2b087e8f881895115259b78a74505d914c3de
PedroBrasil111 <p260637@dac.unicamp.br>
2023-06-18 11:20:22 -0300

    adicionar mp

--- game/src/entidades/Protagonista.java
+++ game/src/entidades/Protagonista.java
@@ -7,12 +7,14 @@ import combate.Ataque;
 import combate.Magia;
 
 public class Protagonista extends Personagem {
+    private int mp; // mana points
     private final Inventario inventario;
     private final List<Magia> listaMagias;
 
     /* Construtor */
-    public Protagonista(String nome, int hp, int def, int atq) {
+    public Protagonista(String nome, int hp, int def, int atq, int mp) {
         super(nome, hp, def, atq);
+        this.mp = mp;
         inventario = new Inventario();
         listaMagias = new ArrayList<Magia>();
     }
@@ -25,11 +27,12 @@ public class Protagonista extends Personagem {
      *     def: <def>
      *     atq: <atq>
      *     listaAtaques: <ataque1.nome>, <ataque2.nome>, ...
+     *     mp: <mp>
      *     listaMagias: <magia1.nome>, <magia2.nome>, ...
      */
     public String toString() {
         String str = super.toString().replaceFirst("Personagem", "Protagonista") +
-            "\n\tlistaMagias:";
+            "\n\tmp: " + mp + "\n\tlistaMagias:";
         for (Magia m: listaMagias)
             str += " " + m.getNome();
         return str;
@@ -42,6 +45,14 @@ public class Protagonista extends Personagem {
 
     public List<Magia> getListaMagias() {
         return listaMagias;
+    }
+
+    public void setMp(int mp) {
+        this.mp = mp;
+    }
+    
+    public int getMp() {
+        return mp;
     }
 
 }
