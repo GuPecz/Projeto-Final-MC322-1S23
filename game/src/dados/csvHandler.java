40a43d6aad17687c36a8b26c7b50e03bdc1bcf7f
PedroBrasil111 <p260637@dac.unicamp.br>
2023-06-18 13:32:56 -0300

    implementar classe

---
+++ game/src/dados/csvHandler.java
@@ -0,0 +1,28 @@
+package dados;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class csvHandler {
+    public static double getMultiplicador(String elementoAtq, String elementoDef) {
+        String tabela = "game/resources/efetividade-elementos-varias-linhas.csv";
+        String strLinha;
+        try (BufferedReader br = new BufferedReader(new FileReader(tabela))) {
+            // Skip the header line
+            br.readLine();
+            while ((strLinha = br.readLine()) != null) {
+                String[] linha = strLinha.split(","); // separa colunas por ","
+                if (linha[0].equals(elementoAtq) && linha[1].equals(elementoDef))
+                    return Double.parseDouble(linha[2]);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return 0.;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getMultiplicador("agua", "fogo"));
+    }
+}
