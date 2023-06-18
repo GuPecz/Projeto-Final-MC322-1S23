ad1a07632193b2bd5fa3acccd1388cd6384d76aa
PedroBrasil111 <p260637@dac.unicamp.br>
2023-06-18 13:33:08 -0300

    implementar calculo de dano

--- game/src/combate/Magia.java
+++ game/src/combate/Magia.java
@@ -1,15 +1,44 @@
 package combate;
 
-public class Magia {
-    private String nome;
+import dados.csvHandler;
+import entidades.Inimigo;
+
+public class Magia extends Ataque {
+    private String elemento;
+    private int custoMp;
+
+    public Magia(String nome, int danoBase, int multiplicador, String elemento, int custoMp) {
+        super(nome, danoBase, multiplicador);
+        this.elemento = elemento;
+        this.custoMp = custoMp;
+    }
+
+    public int danoCausado(Inimigo inimigo) {
+        double multiplicador = csvHandler.getMultiplicador(elemento, inimigo.getElemento());
+        if (multiplicador != 0.) { // retorno padrao da funcao quando nao encontra o elemento
+            // roubei do lol -- https://leagueoflegends.fandom.com/wiki/Magic_resistance?so=search
+            return (int) (getDanoBase() * multiplicador) * (100 / (100 + inimigo.getDef()));
+        }
+        return 0;
+    }
+
+
 
     // Getters e setters
-    public String getNome() {
-        return nome;
+    public String getElemento() {
+        return elemento;
     }
-    
-    public void setNome(String nome) {
-        this.nome = nome;
+
+    public void setElemento(String elemento) {
+        this.elemento = elemento;
+    }
+
+    public int getCustoMp() {
+        return custoMp;
+    }
+
+    public void setCustoMp(int custoMp) {
+        this.custoMp = custoMp;
     }
 
 }
