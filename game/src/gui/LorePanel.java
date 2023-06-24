78175f2b3ab2f2b50168d55e95b2b40362810c00
Gustavo Montenari Pechta <mrpecz@proton.me>
2023-06-24 18:45:11 -0300

    adicionar rascunho do texto a ser exibido

--- game/src/gui/LorePanel.java
+++ game/src/gui/LorePanel.java
@@ -9,7 +9,26 @@ public class LorePanel extends JPanel {
 	 * Create the panel.
 	 */
 	public LorePanel() {
-
+		
 	}
 
 }
+
+/*
+ * Texto da lore
+ * (nome), o reino precisa de você!
+ * 
+ * Terríveis vilões e seus lacaios estão aterrorizando a população.
+ * 
+ * Em tempos dificuldade, um homem ganancioso extorque os necessitados dizendo ter a salvação. 
+ * Pobres almas manipuladas pelo sua suposta habilidade de juntar riquezas o seguem agora.
+ * 
+ * Um aterrador ancião leva a mente de quem o ouve a mais insana confusão.
+ * Tolamente alguns o seguem como discípulos na falsa esperança de entender seus discursos incompreensíveis...
+ * 
+ * Dizem ainda que, nos momentos mais desafortunados, conseguem ouvir uma melodia assustadora e se sentem um arrepio na espinha,
+ * como se a própria morte estivesse a seu lado quando então avistam uma figura ao longe produzindo tais sons.
+ * Mesmo esta criatura conseguiu reunir lacaios que tentam imitá-la.
+ * 
+ * Por favor, eles usam uma torre como covil, invada-a e nos salve!
+ */
